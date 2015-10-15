b07bcf0cd31f2706bcf3f33533a972b0f8289159
descent <descent0@gmail.com>
2015-10-15 10:43:09 +0800

    remove the same definition macro

--- leeos/chapter3/part2/mmu.c
+++ leeos/chapter3/part2/mmu.c
@@ -20,19 +20,18 @@ along with this program.  If not, see http://www.gnu.org/licenses/.
 #include <stdio.h>
 #endif
 
+#define PTE_BITS_L1_SECTION				(0x2)
+#define PTE_L1_SECTION_PADDR_BASE_MASK	(0xfff00000)
+#define PAGE_TABLE_L1_BASE_ADDR_MASK	(0xffffc000)
+#define VIRT_TO_PTE_L1_INDEX(addr)	(((addr) & 0xfff00000) >> 18)
+
 #define RPI2
 
 #ifdef RPI2
-#define PAGE_TABLE_L1_BASE_ADDR_MASK	(0xffffc000)
-
-#define VIRT_TO_PTE_L1_INDEX(addr)	(((addr)&0xfff00000)>>18)
 
 #define PTE_L1_SECTION_NO_CACHE_AND_WB	(0x0<<2)
 #define PTE_L1_SECTION_DOMAIN_DEFAULT	(0x0<<5)
 #define PTE_ALL_AP_L1_SECTION_DEFAULT	(0x1<<10)
-
-#define PTE_L1_SECTION_PADDR_BASE_MASK	(0xfff00000)
-#define PTE_BITS_L1_SECTION				(0x2)
 
 #define L1_PTR_BASE_ADDR			0x30700000
 #define PHYSICAL_MEM_ADDR			0x00000000
@@ -43,16 +42,11 @@ along with this program.  If not, see http://www.gnu.org/licenses/.
 #define IO_MAP_SIZE				0x18000000
 #else
 /*mask for page table base addr*/
-#define PAGE_TABLE_L1_BASE_ADDR_MASK	(0xffffc000)
 
-#define VIRT_TO_PTE_L1_INDEX(addr)	(((addr)&0xfff00000)>>18)
 
 #define PTE_L1_SECTION_NO_CACHE_AND_WB	(0x0<<2)
 #define PTE_L1_SECTION_DOMAIN_DEFAULT	(0x0<<5)
 #define PTE_ALL_AP_L1_SECTION_DEFAULT	(0x1<<10)
-
-#define PTE_L1_SECTION_PADDR_BASE_MASK	(0xfff00000)
-#define PTE_BITS_L1_SECTION				(0x2)
 
 #define L1_PTR_BASE_ADDR			0x30700000
 #define PHYSICAL_MEM_ADDR			0x30000000
@@ -85,13 +79,13 @@ void start_mmu(void){
 #endif
 
 unsigned int gen_l1_pte(unsigned int paddr){
-	return (paddr&PTE_L1_SECTION_PADDR_BASE_MASK)|\
-										PTE_BITS_L1_SECTION;
+	return (paddr & PTE_L1_SECTION_PADDR_BASE_MASK);
 }
 
 unsigned int gen_l1_pte_addr(unsigned int baddr, unsigned int vaddr){
-	return (baddr&PAGE_TABLE_L1_BASE_ADDR_MASK)| VIRT_TO_PTE_L1_INDEX(vaddr);
+	return (baddr & PAGE_TABLE_L1_BASE_ADDR_MASK) | VIRT_TO_PTE_L1_INDEX(vaddr);
 }
+
 
 void init_sys_mmu(void){
 	unsigned int pte;
@@ -99,11 +93,13 @@ void init_sys_mmu(void){
 	int j;
 
 	for(j=0;j<MEM_MAP_SIZE>>20;j++){
-		pte=gen_l1_pte(PHYSICAL_MEM_ADDR+(j<<20));
-		pte|=PTE_ALL_AP_L1_SECTION_DEFAULT;
-		pte|=PTE_L1_SECTION_NO_CACHE_AND_WB;
-		pte|=PTE_L1_SECTION_DOMAIN_DEFAULT;
-		pte_addr=gen_l1_pte_addr(L1_PTR_BASE_ADDR, VIRTUAL_MEM_ADDR+(j<<20));
+		pte = gen_l1_pte(PHYSICAL_MEM_ADDR+(j<<20));
+		pte |= PTE_BITS_L1_SECTION;
+		pte |= PTE_ALL_AP_L1_SECTION_DEFAULT;
+		pte |= PTE_L1_SECTION_NO_CACHE_AND_WB;
+		pte |= PTE_L1_SECTION_DOMAIN_DEFAULT;
+
+		pte_addr = gen_l1_pte_addr(L1_PTR_BASE_ADDR, VIRTUAL_MEM_ADDR+(j<<20));
 #ifdef TEST_MMU
                 printf("%d ## pte_addr: %x, pte: %x\n", j, pte_addr, pte);
 #else
@@ -112,10 +108,10 @@ void init_sys_mmu(void){
 	}
 	for(j=0;j<IO_MAP_SIZE>>20;j++){
 		pte=gen_l1_pte(PHYSICAL_IO_ADDR+(j<<20));
-		pte|=PTE_ALL_AP_L1_SECTION_DEFAULT;
-		pte|=PTE_L1_SECTION_NO_CACHE_AND_WB;
-		pte|=PTE_L1_SECTION_DOMAIN_DEFAULT;
-		pte_addr=gen_l1_pte_addr(L1_PTR_BASE_ADDR, VIRTUAL_IO_ADDR+(j<<20));
+		pte |= PTE_ALL_AP_L1_SECTION_DEFAULT;
+		pte |= PTE_L1_SECTION_NO_CACHE_AND_WB;
+		pte |= PTE_L1_SECTION_DOMAIN_DEFAULT;
+		pte_addr = gen_l1_pte_addr(L1_PTR_BASE_ADDR, VIRTUAL_IO_ADDR+(j<<20));
 #ifdef TEST_MMU
                 printf("%d (IO) ## pte_addr: %x, pte: %x\n", j, pte_addr, pte);
 #else
